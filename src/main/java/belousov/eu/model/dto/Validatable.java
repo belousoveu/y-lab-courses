b2b33dc5bcd2912d805f5dc0bb40eadb5a011e6a
Belousoveu <belousov.eu@gmail.com>
2025-03-21 08:42:42 +0300

    feat: Add validation for request fields

---
+++ src/main/java/belousov/eu/model/dto/Validatable.java
@@ -0,0 +1,4 @@
+package belousov.eu.model.dto;
+
+public interface Validatable {
+}
--- src/main/java/belousov/eu/servlet/AuthServlet.java
+++ src/main/java/belousov/eu/servlet/AuthServlet.java
@@ -5,15 +5,21 @@ import belousov.eu.exception.InvalidPasswordException;
 import belousov.eu.model.User;
 import belousov.eu.model.dto.LoginDto;
 import belousov.eu.model.dto.RegisterDto;
+import belousov.eu.model.dto.Validatable;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.io.IOException;
+import java.util.Set;
 
 @RequiredArgsConstructor
 public class AuthServlet extends HttpServlet {
@@ -29,12 +35,22 @@ public class AuthServlet extends HttpServlet {
         String[] parts = path.split("/");
 
         HttpSession session = request.getSession();
+        Set<ConstraintViolation<Validatable>> violations;
 
-        try {
+        try (ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory()) {
+            Validator validator = validatorFactory.getValidator();
             if (parts.length > 1) {
                 switch (parts[1]) {
                     case "login":
                         LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
+
+                        violations = validator.validate(loginDto);
+
+                        if (!violations.isEmpty()) {
+                            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                            response.getWriter().write("Указаны неверные данные: " + violations);
+                            return;
+                        }
                         User authUser = authController.login(loginDto);
                         session.setAttribute("currentUser", authUser);
 
@@ -43,6 +59,14 @@ public class AuthServlet extends HttpServlet {
                         break;
                     case "register":
                         RegisterDto registerDto = objectMapper.readValue(request.getInputStream(), RegisterDto.class);
+
+                        violations = validator.validate(registerDto);
+
+                        if (!violations.isEmpty()) {
+                            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                            response.getWriter().write("Указаны неверные данные: " + violations);
+                            return;
+                        }
                         User newUser = authController.register(registerDto);
                         session.setAttribute("currentUser", newUser);
 
